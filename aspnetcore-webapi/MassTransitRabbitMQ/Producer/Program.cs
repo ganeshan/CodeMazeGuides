4d6fd086ad3ea00fccda88f7b5bdced223167bbb
Ganeshan Venkataraman <ganeshanv@msn.com>
2023-05-17 19:44:20 -0400

    Add Swagger UI

--- aspnetcore-webapi/MassTransitRabbitMQ/Producer/Program.cs
+++ aspnetcore-webapi/MassTransitRabbitMQ/Producer/Program.cs
@@ -1,18 +1,29 @@
 using MassTransit;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddMassTransit(x =>
-{
-    x.UsingRabbitMq();
-});
+builder.Services.AddMassTransit(x => { x.UsingRabbitMq((_, _) => { }); });
 
-builder.Services.AddMassTransitHostedService();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "MassTransit Producer", Version = "v1"
+    });
+});
+// builder.Services.AddMassTransitHostedService();
 
 builder.Services.AddControllers();
 
 var app = builder.Build();
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 // Configure the HTTP request pipeline.
 
